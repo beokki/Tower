b78f6a5e296ba1d02fd60acef49ea66065b69f9d
beokki <122079885+beokki@users.noreply.github.com>
2024-04-29 04:50:42 -0400

    Useless

--- Assets/Scripts/TurretPlacement.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public static  class TurretPlacement
-{
-    public static GameObject SelectedTurretPrefab {  get; set; }
-}
